a975c44a68eb7ed36be921b4d899dcaf8af66a1d
Mahesh <maheshsolanke1122@gmail.com>
2024-02-13 13:05:24 +0530

    updated database connection url

--- crud-nodejs/backend/config/db.js
+++ crud-nodejs/backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = () => {
   try {
-    const conn = mongoose.connect("mongodb://localhost:");
+    const conn = mongoose.connect("your mongoDB url");
 
     console.log("Connecting to" + conn.connection.host);
   } catch (err) {
